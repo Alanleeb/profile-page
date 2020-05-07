81f7359613d068a58b383affdea5bb8009048c59
Alan Bischoff <alan_bischoff@Alans-MacBook-Pro-2019.local>
2020-05-07 12:46:01 -0600

    Updates

---
+++ precache-manifest.9cd6e1c66a03196cf4da5a0bc66ca197.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934a5d070d369354f9838388e2a42e9e",
+    "url": "/portfolio-page/index.html"
+  },
+  {
+    "revision": "c442a95018dd763591b6",
+    "url": "/portfolio-page/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "3635737574c25b060fea",
+    "url": "/portfolio-page/static/css/main.9614144f.chunk.css"
+  },
+  {
+    "revision": "c442a95018dd763591b6",
+    "url": "/portfolio-page/static/js/2.16aad4f6.chunk.js"
+  },
+  {
+    "revision": "2293e6a98171026a91c6cc0113a68b12",
+    "url": "/portfolio-page/static/js/2.16aad4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3635737574c25b060fea",
+    "url": "/portfolio-page/static/js/main.942f7374.chunk.js"
+  },
+  {
+    "revision": "58c08f2cf09156f53454",
+    "url": "/portfolio-page/static/js/runtime-main.9f15cfcf.js"
+  },
+  {
+    "revision": "cef54c9fd999a7f843ffc5a7651bfb8c",
+    "url": "/portfolio-page/static/media/logo.cef54c9f.jpg"
+  },
+  {
+    "revision": "b3b7fdc55d5d0580e8d0dd2daa711996",
+    "url": "/portfolio-page/static/media/nature-illustration.b3b7fdc5.jpg"
+  },
+  {
+    "revision": "610fd4a191e0d6df5f11295b39abd26c",
+    "url": "/portfolio-page/static/media/profile-pic.610fd4a1.jpg"
+  }
+]);
